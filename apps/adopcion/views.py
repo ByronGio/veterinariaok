c5c9c09fb7a6d913a01a89d1335371e6342a7ba5
ByronGio <byrong.santos@gmail.com>
2016-11-24 14:17:04 -0600

    CSS

--- apps/adopcion/views.py
+++ apps/adopcion/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
